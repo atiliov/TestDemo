75df70308272804b2927f2a23d49056ad4bf2647
Atilio Vivern <atilio@bairesqa.com>
2018-04-07 12:07:40 -0300

    Solo 3 test, 2 fallan uno no.

--- src/test/java/TestMailG.java
+++ src/test/java/TestMailG.java
@@ -5,13 +5,34 @@ import org.testng.annotations.Test;
 public class TestMailG {
 
     @Test()
-    public void testEmailGenerator() {
+    public void testEmailGeneratorNoVaAFallar() {
 
         RandomEmailGenerator obj = new RandomEmailGenerator();
         String email = obj.generate();
 
         Assert.assertNotNull(email);
         Assert.assertEquals(email, "feedback@yoursite.com");
+
+    }
+    @Test()
+    public void testEmailGeneratorFallara() {
+
+        RandomEmailGenerator obj = new RandomEmailGenerator();
+        String email = null;
+
+        Assert.assertNotNull(email);
+        Assert.assertEquals(email, "feedback@yoursite.com");
+
+    }
+
+    @Test()
+    public void testEmailGeneratorFallaraDeNuevo() {
+
+        RandomEmailGenerator obj = new RandomEmailGenerator();
+        String email = obj.generate();
+
+        Assert.assertNotNull(email);
+        Assert.assertEquals(email, "feedback@NiEnPedo.com");
 
     }
 
